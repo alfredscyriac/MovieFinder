1315bae2afba598418439c74f28a364f41c4518e
Alfred <alfredscyriac@gmail.com>
2025-06-26 11:43:15 -0400

    create update search count function to track how many times users have searched for unique movies utilizing appwrite as the database

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from 'appwrite'; 
+import { Client, Databases, ID, Query } from 'appwrite'; 
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID; 
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID; 
@@ -24,7 +24,13 @@ export const updateSearchCount = async(searchTerm, movie) => {
             await database.updateDocument(DATABASE_ID. COLLECTION_ID, doc.$id, {
                 count: doc.count + 1,
             })
-
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            })
         }
 
     } catch(error) {
